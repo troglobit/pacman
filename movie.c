e8e12d0f2b5f009de1094e0f3cec3dd3009c15bc
Joachim Nilsson <troglobit@gmail.com>
2019-05-19 12:11:58 +0200

    Minor, cleanup functions declarations + missing includes
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- movie.c
+++ movie.c
@@ -25,6 +25,7 @@
  */
 #include "pacdefs.h"
 #include <curses.h>
+#include <unistd.h>
 
 /* This is the monster we use.  Don't take out the extra spaces here, 
  * they are there so that the thing "cleans up" after itself, since 
@@ -37,7 +38,214 @@ char *bigmonster[] = {
 	"  v^v^v^v   ",
 };
 
-movie()
+static void left1(void)
+{
+	SPLOT(10, 0, "_____    \n"); 
+	SPLOT(11, 0, "     \\   \n");
+	SPLOT(12, 0, " O O |   \n"); 
+	SPLOT(13, 0, "^v^v^v   \n");
+	syncscreen();
+}
+
+static void left2(void)
+{
+	SPLOT(10, 0, "____    \n"); 
+	SPLOT(11, 0, "    \\   \n");
+	SPLOT(12, 0, "O O |   \n"); 
+ 	SPLOT(13, 0, "v^v^v   \n");
+	syncscreen();
+}
+
+static void left3(void)
+{
+	SPLOT(10, 0, "___    \n"); 
+	SPLOT(11, 0, "   \\   \n");
+	SPLOT(12, 0, " O |   \n"); 
+	SPLOT(13, 0, "^v^v   \n");
+	syncscreen();
+}
+
+static void left4(void)
+{
+	SPLOT(10, 0, "__    \n"); 
+	SPLOT(11, 0, "  \\   \n");
+	SPLOT(12, 0, "O |   \n"); 
+	SPLOT(13, 0, "v^v   \n");
+	syncscreen();
+}
+
+static void left5(void)
+{
+	SPLOT(10, 0, "_    \n"); 
+	SPLOT(11, 0, " \\   \n");
+	SPLOT(12, 0, " |   \n"); 
+	SPLOT(13, 0, "^v   \n");
+	syncscreen();
+}
+
+static void left6(void)
+{
+	SPLOT(10, 0, "    \n"); 
+	SPLOT(11, 0, "\\   \n");
+	SPLOT(12, 0, "|   \n"); 
+	SPLOT(13, 0, "v   \n");
+	syncscreen();
+}
+
+static void left7(void)
+{
+	SPLOT(10, 0, "   \n"); 
+	SPLOT(11, 0, "   \n");
+	SPLOT(12, 0, "   \n"); 
+	SPLOT(13, 0, "   \n");
+	syncscreen();
+}
+
+static void right1(void)
+{
+	SPLOT(10, 69, "   _____ "); 
+	SPLOT(11, 69, "  /     \\");
+	SPLOT(12, 69, "  | O O |"); 
+	SPLOT(13, 69, "  v^v^v^v");
+}
+
+static void right2(void)
+{
+	SPLOT(10, 70, "   _____"); 
+	SPLOT(11, 70, "  /     ");
+	SPLOT(12, 70, "  | O O "); 
+	SPLOT(13, 70, "  v^v^v^");
+}
+
+static void right3(void)
+{
+	SPLOT(10, 71, "   ____"); 
+	SPLOT(11, 71, "  /    ");
+	SPLOT(12, 71, "  | O O"); 
+	SPLOT(13, 71, "  v^v^v");
+}
+
+static void right4(void)
+{
+	SPLOT(10, 72, "   ___"); 
+	SPLOT(11, 72, "  /   ");
+	SPLOT(12, 72, "  | O "); 
+	SPLOT(13, 72, "  v^v^");
+}
+
+static void right5(void)
+{
+	SPLOT(10, 73, "   __"); 
+	SPLOT(11, 73, "  /  ");
+	SPLOT(12, 73, "  | O"); 
+	SPLOT(13, 73, "  v^v");
+}
+
+static void right6(void)
+{
+	SPLOT(10, 74, "   _"); 
+	SPLOT(11, 74, "  / ");
+	SPLOT(12, 74, "  | "); 
+	SPLOT(13, 74, "  v^");
+}
+
+static void right7(void)
+{
+	SPLOT(10, 75, "   "); 
+	SPLOT(11, 75, "  /");
+	SPLOT(12, 75, "  |"); 
+	SPLOT(13, 75, "  v");
+}
+
+static void right8(void)
+{
+	SPLOT(10, 76, " ");
+	SPLOT(11, 76, " ");
+	SPLOT(12, 76, " ");
+	SPLOT(13, 76, " ");
+}
+
+static void right9(void)
+{
+	SPLOT(10, 77, " ");
+	SPLOT(11, 77, " ");
+	SPLOT(12, 77, " ");
+	SPLOT(13, 77, " ");
+}
+
+static void monst7(xxx)
+int xxx;
+{
+	SPLOT(10, xxx, "     <");
+}
+
+static void monst6(xxx)
+int xxx;
+{
+	SPLOT( 9, xxx, "     /");
+	SPLOT(10, xxx, "    < ");
+	SPLOT(11, xxx, "     \\");
+}
+
+static void monst5(xxx)
+int xxx;
+{
+	SPLOT( 8, xxx, "     /");
+	SPLOT( 9, xxx, "    / ");
+	SPLOT(10, xxx, "   <  ");
+	SPLOT(11, xxx, "    \\ ");
+	SPLOT(12, xxx, "     \\");
+}
+
+static void monst4(xxx)
+int xxx;
+{
+	SPLOT( 7, xxx, "     /");
+	SPLOT( 8, xxx, "    / ");
+	SPLOT( 9, xxx, "   /  ");
+	SPLOT(10, xxx, "  <   ");
+	SPLOT(11, xxx, "   \\   ");
+	SPLOT(12, xxx, "    \\  ");
+	SPLOT(13, xxx, "     \\ ");
+}
+
+static void monst3(xxx)
+int xxx;
+{
+	SPLOT(7 , xxx, "    / ");
+	SPLOT(8 , xxx, "   /  ");
+	SPLOT(9 , xxx, "  /   ");
+	SPLOT(10, xxx, "      ");
+	SPLOT(11, xxx, "  \\   ");
+	SPLOT(12, xxx, "   \\  ");
+	SPLOT(13, xxx, "    \\ ");
+}
+
+static void monst2(xxx)
+int xxx;
+{
+	SPLOT( 7, xxx, "   / ");
+	SPLOT( 8, xxx, "  /  ");
+	SPLOT( 9, xxx, "     ");
+	SPLOT(10, xxx, "     ");
+	SPLOT(11, xxx, "     ");
+	SPLOT(12, xxx, "  \\  ");
+	SPLOT(13, xxx, "   \\ ");
+}
+
+static void monst1(xxx)
+int xxx;
+{
+	SPLOT( 7, xxx, "  / ");
+	SPLOT( 8, xxx, "    ");
+	SPLOT( 9, xxx, "    ");
+	SPLOT(10, xxx, "    ");
+	SPLOT(11, xxx, "    ");
+	SPLOT(12, xxx, "    ");
+	SPLOT(13, xxx, "  \\ ");
+}
+
+int movie(void)
 {
 	int i, j, k;
 
@@ -45,7 +253,7 @@ movie()
 	syncscreen();
 
 	/*
-	 * this loop moves the monster and the small pacman from right to 
+	 * this loop moves the monster and the small pacman from right to
 	 * left, until it's time to start printing "fractional" monsters
 	 */
 	for (i=70; i > (-1); i--) {
@@ -65,21 +273,21 @@ movie()
 	 * left1-left7 are "partial" monster routines, they are also called when
 	 * the monster comes back on from the left (called in opposite order)
 	 */
-	left1(); left2(); 
-	left3(); left4(); 
-	left5(); left6(); 
-	left7(); 
+	left1(); left2();
+	left3(); left4();
+	left5(); left6();
+	left7();
 
 	sleep(1);
 
 	/* Now we come back on...  */
-	left7(); left6(); 
-	left5(); left4(); 
-	left3(); left2(); 
-	left1(); 
+	left7(); left6();
+	left5(); left4();
+	left3(); left2();
+	left1();
 
 	/*
-	 * Now we start moving the big pacman across.  monst1-monst3 are 
+	 * Now we start moving the big pacman across.  monst1-monst3 are
 	 * routines for drawing pieces of the pacman, until the whole thing
 	 * is on the screen.
 	 */
@@ -106,7 +314,7 @@ movie()
 		syncscreen();
 	}
 	/*
-	 * right1-right9 are partial monster routines, for moving him off to 
+	 * right1-right9 are partial monster routines, for moving him off to
 	 * the right of the screen.  monst4 prints the whole pacman.
 	 */
 	right1(); monst4(50); syncscreen();
@@ -133,188 +341,4 @@ movie()
 	/* clean up a little bit */
 	clear();
 	syncscreen();
-}
-left1()
-{
-	SPLOT(10, 0, "_____    \n"); 
-	SPLOT(11, 0, "     \\   \n");
-	SPLOT(12, 0, " O O |   \n"); 
-	SPLOT(13, 0, "^v^v^v   \n");
-	syncscreen();
-}
-left2()
-{
-	SPLOT(10, 0, "____    \n"); 
-	SPLOT(11, 0, "    \\   \n");
-	SPLOT(12, 0, "O O |   \n"); 
- 	SPLOT(13, 0, "v^v^v   \n");
-	syncscreen();
-}
-left3()
-{
-	SPLOT(10, 0, "___    \n"); 
-	SPLOT(11, 0, "   \\   \n");
-	SPLOT(12, 0, " O |   \n"); 
-	SPLOT(13, 0, "^v^v   \n");
-	syncscreen();
-}
-left4()
-{
-	SPLOT(10, 0, "__    \n"); 
-	SPLOT(11, 0, "  \\   \n");
-	SPLOT(12, 0, "O |   \n"); 
-	SPLOT(13, 0, "v^v   \n");
-	syncscreen();
-}
-left5()
-{
-	SPLOT(10, 0, "_    \n"); 
-	SPLOT(11, 0, " \\   \n");
-	SPLOT(12, 0, " |   \n"); 
-	SPLOT(13, 0, "^v   \n");
-	syncscreen();
-}
-left6()
-{
-	SPLOT(10, 0, "    \n"); 
-	SPLOT(11, 0, "\\   \n");
-	SPLOT(12, 0, "|   \n"); 
-	SPLOT(13, 0, "v   \n");
-	syncscreen();
-}
-left7()
-{
-	SPLOT(10, 0, "   \n"); 
-	SPLOT(11, 0, "   \n");
-	SPLOT(12, 0, "   \n"); 
-	SPLOT(13, 0, "   \n");
-	syncscreen();
-}
-right1()
-{
-	SPLOT(10, 69, "   _____ "); 
-	SPLOT(11, 69, "  /     \\");
-	SPLOT(12, 69, "  | O O |"); 
-	SPLOT(13, 69, "  v^v^v^v");
-}
-right2()
-{
-	SPLOT(10, 70, "   _____"); 
-	SPLOT(11, 70, "  /     ");
-	SPLOT(12, 70, "  | O O "); 
-	SPLOT(13, 70, "  v^v^v^");
-}
-right3()
-{
-	SPLOT(10, 71, "   ____"); 
-	SPLOT(11, 71, "  /    ");
-	SPLOT(12, 71, "  | O O"); 
-	SPLOT(13, 71, "  v^v^v");
-}
-right4()
-{
-	SPLOT(10, 72, "   ___"); 
-	SPLOT(11, 72, "  /   ");
-	SPLOT(12, 72, "  | O "); 
-	SPLOT(13, 72, "  v^v^");
-}
-right5()
-{
-	SPLOT(10, 73, "   __"); 
-	SPLOT(11, 73, "  /  ");
-	SPLOT(12, 73, "  | O"); 
-	SPLOT(13, 73, "  v^v");
-}
-right6()
-{
-	SPLOT(10, 74, "   _"); 
-	SPLOT(11, 74, "  / ");
-	SPLOT(12, 74, "  | "); 
-	SPLOT(13, 74, "  v^");
-}
-right7()
-{
-	SPLOT(10, 75, "   "); 
-	SPLOT(11, 75, "  /");
-	SPLOT(12, 75, "  |"); 
-	SPLOT(13, 75, "  v");
-}
-right8()
-{
-	SPLOT(10, 76, " ");
-	SPLOT(11, 76, " ");
-	SPLOT(12, 76, " ");
-	SPLOT(13, 76, " ");
-}
-right9()
-{
-	SPLOT(10, 77, " ");
-	SPLOT(11, 77, " ");
-	SPLOT(12, 77, " ");
-	SPLOT(13, 77, " ");
-}
-monst7(xxx)
-int xxx;
-{
-	SPLOT(10, xxx, "     <");
-}
-monst6(xxx)
-int xxx;
-{
-	SPLOT( 9, xxx, "     /");
-	SPLOT(10, xxx, "    < ");
-	SPLOT(11, xxx, "     \\");
-}
-monst5(xxx)
-int xxx;
-{
-	SPLOT( 8, xxx, "     /");
-	SPLOT( 9, xxx, "    / ");
-	SPLOT(10, xxx, "   <  ");
-	SPLOT(11, xxx, "    \\ ");
-	SPLOT(12, xxx, "     \\");
-}
-monst4(xxx)
-int xxx;
-{
-	SPLOT( 7, xxx, "     /");
-	SPLOT( 8, xxx, "    / ");
-	SPLOT( 9, xxx, "   /  ");
-	SPLOT(10, xxx, "  <   ");
-	SPLOT(11, xxx, "   \\   ");
-	SPLOT(12, xxx, "    \\  ");
-	SPLOT(13, xxx, "     \\ ");
-}
-monst3(xxx)
-int xxx;
-{
-	SPLOT(7 , xxx, "    / ");
-	SPLOT(8 , xxx, "   /  ");
-	SPLOT(9 , xxx, "  /   ");
-	SPLOT(10, xxx, "      ");
-	SPLOT(11, xxx, "  \\   ");
-	SPLOT(12, xxx, "   \\  ");
-	SPLOT(13, xxx, "    \\ ");
-}
-monst2(xxx)
-int xxx;
-{
-	SPLOT( 7, xxx, "   / ");
-	SPLOT( 8, xxx, "  /  ");
-	SPLOT( 9, xxx, "     ");
-	SPLOT(10, xxx, "     ");
-	SPLOT(11, xxx, "     ");
-	SPLOT(12, xxx, "  \\  ");
-	SPLOT(13, xxx, "   \\ ");
-}
-monst1(xxx)
-int xxx;
-{
-	SPLOT( 7, xxx, "  / ");
-	SPLOT( 8, xxx, "    ");
-	SPLOT( 9, xxx, "    ");
-	SPLOT(10, xxx, "    ");
-	SPLOT(11, xxx, "    ");
-	SPLOT(12, xxx, "    ");
-	SPLOT(13, xxx, "  \\ ");
 }
